67226d3c835684cc822ac391415601e1bad367f6
khanhdrag9 <tkhanh783@gmail.com>
2019-07-19 17:32:51 +0700

    !init

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    public Vector2 speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Horizontal
+        float horizontal = Input.GetAxis("Horizontal");
+        float x = transform.position.x + speed.x * horizontal * Time.deltaTime;
+
+        //Verticale
+        float vertical = Input.GetAxis("Vertical");
+        float y = transform.position.y + speed.y * vertical * Time.deltaTime;
+
+        transform.position = new Vector2(x, y);
+    }
+}
